605597e1f0c9014df34bc455434223d3fd68ae6b
Muhammad Ikhsan Asa Pambayun <ikhsan.pambayun@gmail.com>
2023-05-01 12:10:03 +0700

    enable pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,36 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Biskita Transpakuan Bogor",
+        short_name: "Biskita",
+        description: "Biskita Transpakuan Bogor by Rheznandyae",
+        start_url: `/`,
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        display: "standalone",
+        icon: "src/assets/logo192.png",
+        icons: [
+          {
+            src: "src/assets/logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "src/assets/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-offline",
+      options: {
+        precachePages: ["/"],
+      },
+    },
   ],
 };
